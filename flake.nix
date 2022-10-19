0eb2d336f184f876403df9d9a8839e76a72afe46
stove <stove@rivosinc.com>
2022-10-18 23:13:22 -0700

    add nixConfig with substituters

--- flake.nix
+++ flake.nix
@@ -3,6 +3,11 @@
 
   inputs.nixpkgs.url = "github:picostove/nixpkgs?ref=dev/stove/riscv64-cross-boot-test";
 
+  nixConfig = {
+    extra-substituters = ["s3://rivos-nix-cache?region=us-west-1"];
+    extra-trusted-public-keys = ["nix-cache.ba.rivosinc.com-1:+VQgHlJeU5uI2xaO9EpWMijpXSXUiijgmPWjCWPgTC4="];
+  };
+
   outputs = {
     self,
     nixpkgs,
